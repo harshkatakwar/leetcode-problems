98b2d2d8cf628f88ed552b5ed1edf20549527ab3
Harsh Katakwar <katakwarharsh@gmail.com>
2025-01-10 10:18:12 +0530

    Time: 0 ms (100.00%) | Memory: 21.5 MB (76.99%) - LeetSync

---
+++ 268-missing-number/missing-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+	int missingNumber(vector<int>& nums) {
+		
+		int n = nums.size();
+		int sum = n * (n + 1) / 2;
+		int temp_sum = 0;
+		for (int i = 0; i < n; i++) {
+			temp_sum += nums[i];
+		}
+
+		int diff = sum - temp_sum;
+		return diff;
+	}
+};
